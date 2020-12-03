0936b7db6136379b625f5b94fec4bb09d0cd9326
ggarber <ggarber@protonmail.com>
2020-12-02 22:36:11 -0300

    remove particles from mobile

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -5,65 +5,69 @@ import Particles from "react-particles-js"
 import * as S from "./styled"
 
 const Intro = () => {
-
-  const [particleNumber, setParticleNumber] = useState(150);
+  const [showParticles, setShowParticle] = useState(true)
   useEffect(() => {
-    if(window.innerWidth < 800){
-      setParticleNumber(35);
-    } else if (window.innerWidth < 600){
-      setParticleNumber(1);
+    if (window.innerWidth < 600) {
+      setShowParticle(false)
+    } else {
+      setShowParticle(true)
     }
-  }, []);
+  }, [])
 
-  return(
-  <S.Section>
-    <Particles
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        opacity: 0.2,
-      }}
-      params={{
-        particles: {
-          number: {
-            value: particleNumber,
-          },
-          size: {
-            value: 3,
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: true,
-              mode: "repulse",
+  return (
+    <S.Section>
+      {showParticles ? (
+        <Particles
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            opacity: 0.2,
+          }}
+          params={{
+            particles: {
+              number: {
+                value: 150,
+              },
+              size: {
+                value: 3,
+              },
             },
-          },
-        },
-      }}
-    />
-    <S.StyledH1>
-      Bem vindo ao meu <span>pequeno</span> espaço na internet
-    </S.StyledH1>
-    <S.TypeWriterH2>
-      <Typed
-        strings={[
-          "Aqui falo",
-          "Aqui escrevo sobre",
-          "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>Javascript</span>",
-          "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>Python</span>",
-          "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>PHP</span>",
-          "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>tecnologias</span>",
-        ]}
-        typeSpeed={30}
-        backSpeed={40}
-        startDelay={100}
-      />
-    </S.TypeWriterH2>
-  </S.Section>
-)}
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+            },
+          }}
+        />
+      ) : (
+        ""
+      )}
+      <S.StyledH1>
+        Bem vindo ao meu <span>pequeno</span> espaço na internet
+      </S.StyledH1>
+      <S.TypeWriterH2>
+        <Typed
+          strings={[
+            "Aqui falo",
+            "Aqui escrevo sobre",
+            "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>Javascript</span>",
+            "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>Python</span>",
+            "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>PHP</span>",
+            "Aqui escrevo sobre<br><span style='color:var(--clr-accent);font-weight:900;'>tecnologias</span>",
+          ]}
+          typeSpeed={30}
+          backSpeed={40}
+          startDelay={100}
+        />
+      </S.TypeWriterH2>
+    </S.Section>
+  )
+}
 
 export default Intro
